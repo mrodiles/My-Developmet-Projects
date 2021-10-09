b920f4d2952a3a31568d42d154cf7481edd290c4
“Michael <michaelrodiles@gmail.com>
2021-10-08 23:13:05 -0400

    first commit

---
+++ src/components/Sidebar/SidebarElements.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+import { FaTimes } from "react-icons/fa";
+
+export const SidebarContainer = styled.aside`
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  background: #0d0d0d;
+  display: grid;
+  align-items: center;
+  top: 0;
+  left: 0;
+  transition: 0.3s ease-in-out;
+
+  top: 0;
+`;
+
+export const CloseIcon = styled(FaTimes)`
+  color: #0fff;
+`;
+
+export const Icon = styled.div`
+  position: absolute;
+  top: 1.2rem;
+  right: 1.5rem;
+  background: transparent;
+  font-size: 2rem;
+  cursor: pointer;
+  outline: none;
+`;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SidebarContainer, Icon, CloseIcon } from "./SideBarElements";
+
+const SideBarElements = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+    </SidebarContainer>
+  );
+};
+
+export default SideBarElements;
